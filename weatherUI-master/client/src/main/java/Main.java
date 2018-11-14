a5511486c478ca1ae147872888bd9e19bc8fb701
aSushiboi <matthew.chan5@uoit.net>
2018-11-14 12:39:52 -0500

    Made windows borderless and undraggable

--- weatherUI-master/client/src/main/java/Main.java
+++ weatherUI-master/client/src/main/java/Main.java
@@ -10,8 +10,9 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.stage.WindowEvent;
-
+import javafx.scene.input.MouseEvent;
 import java.io.*;
 import java.net.Socket;
 import java.text.DateFormat;
@@ -21,8 +22,11 @@ import java.util.HashMap;
 import java.util.Map;
 
 
+
 public class Main extends Application {
 
+    private double xOffset = 0;
+    private double yOffset = 0;
     @Override
     public void start(Stage primaryStage) throws Exception {
         try {
@@ -31,8 +35,24 @@ public class Main extends Application {
             InputStreamReader ir = new InputStreamReader(clientSocket.getInputStream());
             BufferedReader br = new BufferedReader(ir);
             primaryStage.setTitle("Weather stuff");
+            primaryStage.initStyle(StageStyle.UNDECORATED);
             Parent root = FXMLLoader.load(getClass().getResource("res/sample.fxml"));
             Scene scene = new Scene(root, 490, 630);
+            root.setOnMousePressed(new EventHandler<MouseEvent>() {
+                @Override 
+                public void handle(MouseEvent event){
+                    xOffset = event.getSceneX();
+                    yOffset = event.getSceneY();
+                }
+            });
+
+            root.setOnMouseDragged(new EventHandler<MouseEvent>(){
+                @Override
+                public void handle(MouseEvent event){
+                    primaryStage.setX(event.getScreenX() - xOffset);
+                    primaryStage.setY(event.getScreenY()- yOffset);
+                }
+            });
             primaryStage.setScene(scene);
             primaryStage.setResizable(false);
             primaryStage.show();
